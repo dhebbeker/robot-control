34349dd0e9e634c2b5fbe942dbb78c391fe7f1d6
David Hebbeker <david@hebbeker.info>
2021-05-03 22:28:52 +0200

    Add overload for digitalPinToInterrupt() and analogWrite().

--- robot-control-src/DigitalPin.hpp
+++ robot-control-src/DigitalPin.hpp
@@ -44,6 +44,30 @@ public:
 };
 
 template<std::uint8_t PIN>
+void analogWrite(const DigitalPin<PIN>& gpio, const int dutyCycle)
+{
+	analogWrite(gpio.pin, dutyCycle);
+}
+
+inline constexpr int digitalPinToInterruptWrapper(const int pinNumber)
+{
+	return digitalPinToInterrupt(pinNumber);
+}
+
+#undef digitalPinToInterrupt
+
+inline constexpr int digitalPinToInterrupt(const int pinNumber)
+{
+	return digitalPinToInterruptWrapper(pinNumber);
+}
+
+template<std::uint8_t PIN>
+int constexpr digitalPinToInterrupt(const DigitalPin<PIN>& gpio)
+{
+	return digitalPinToInterruptWrapper(gpio.pin);
+}
+
+template<std::uint8_t PIN>
 void attachInterrupt(const DigitalPin<PIN>& gpio, void (* const interruptServiceRoutine)(void), const int mode)
 {
 	attachInterrupt(digitalPinToInterrupt(gpio.pin), interruptServiceRoutine, mode);
