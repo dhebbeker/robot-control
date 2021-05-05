e58887c72cef9c353e5610e59b6e7364824179b8
David Hebbeker <david@hebbeker.info>
2021-05-05 09:01:41 +0200

    Set correct I2C address for IO expander.

--- robot-control-src/board.cpp
+++ robot-control-src/board.cpp
@@ -2,7 +2,7 @@
 #include <Arduino.h>
 
 namespace board {
-constexpr std::uint8_t ioExpander1Address = 0x00;
+constexpr std::uint8_t ioExpander1Address = 0x20;
 MCP23017 ioExpander1(ioExpander1Address);
 MCP23017Pin leftBackwards(ioExpander1, 8+0);
 MCP23017Pin rightBackwards(ioExpander1, 8+1);
