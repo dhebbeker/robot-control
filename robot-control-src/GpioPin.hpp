125e65e61bcaff7d92fa96709bc86379e2169a36
David Hebbeker <david@hebbeker.info>
2021-05-23 14:45:47 +0200

    Document GpioInterface and GpioPin.

--- robot-control-src/GpioPin.hpp
+++ robot-control-src/GpioPin.hpp
@@ -1,77 +1,130 @@
-#ifndef ROBOT_CONTROL_SRC_DIGITALPIN_HPP_
-#define ROBOT_CONTROL_SRC_DIGITALPIN_HPP_
+#ifndef GPIOPIN_HPP_
+#define GPIOPIN_HPP_
 
 #include "GpioInterface.hpp"
 #include <Arduino.h>
 
+/**
+ * Extension of GpioInterface for the processors general purpose input/output pins.
+ *
+ * Adds method to set PWM duty cycle.
+ *
+ * @tparam PIN is the Arduino pin number
+ */
 template<std::uint8_t PIN>
-class DigitalPin: public GpioInterface {
+class GpioPin: public GpioInterface
+{
 public:
 	static constexpr std::uint8_t pin = PIN;
 
 	virtual int read() override
 	{
-		return readS();
-	}
-	virtual void setMode(const std::uint8_t mode) override
-	{
-		setModeS(mode);
-	}
-	virtual void write(const std::uint8_t value) override
-	{
-		writeS(value);
-	}
-	void setDutyCycle(const int dutyCycle) const
-	{
-		setDutyCycleS(dutyCycle);
-	}
-	static int readS()
-	{
 		return digitalRead(pin);
 	}
-	static void setModeS(const std::uint8_t mode)
+
+	virtual void setMode(const std::uint8_t mode) override
 	{
 		pinMode(pin, mode);
 	}
-	static void writeS(const std::uint8_t value)
+
+	virtual void write(const std::uint8_t value) override
 	{
 		digitalWrite(pin, value);
 	}
-	static void setDutyCycleS(const int dutyCycle)
+
+	/**
+	 * Sets PWM duty cycle.
+	 * @param dutyCycle between 0 and 255
+	 */
+	void setDutyCycle(const int dutyCycle) const
 	{
 		analogWrite(pin, dutyCycle);
 	}
 };
 
-template<std::uint8_t PIN>
-void analogWrite(const DigitalPin<PIN>& gpio, const int dutyCycle)
-{
-	analogWrite(gpio.pin, dutyCycle);
-}
+/* If digitalPinToInterrupt is defined as C preprocessor macro it needs to be
+ * undefined. digitalPinToInterrupt must be overloaded for GpioPin. This is
+ * not possible for macros.
+ */
+#if defined(digitalPinToInterrupt)
 
+/**
+ * Wrapper for digitalPinToInterrupt.
+ *
+ * Encapsulates the digitalPinToInterrupt functionality before it is undefined.
+ * In contrast to the C preprocessor macro this function can be overridden.
+ *
+ * @param pinNumber is the Arduino pin number
+ * @return interrupt number
+ */
 inline constexpr int digitalPinToInterruptWrapper(const int pinNumber)
 {
 	return digitalPinToInterrupt(pinNumber);
 }
 
+/* Undefine macro in order to enable function declaration with the same name. */
 #undef digitalPinToInterrupt
 
+/**
+ * Getter for the interrupt number for a digital input pin.
+ *
+ * This is an adapter to digitalPinToInterruptWrapper().
+ * @param pinNumber is the Arduino pin number
+ * @return interrupt number
+ */
 inline constexpr int digitalPinToInterrupt(const int pinNumber)
 {
 	return digitalPinToInterruptWrapper(pinNumber);
 }
 
+#endif /* defined(digitalPinToInterrupt) */
+
+/** @name Function overload for GpioPin objects.
+ *
+ *  These functions provide an interface to GpioPin objects compatible
+ *  to the Arduino library functions.
+ */
+/** @{ */ /* Start group for function overloads */
+
+/**
+ * Getter for the interrupt number for a digital input pin.
+ *
+ * This is an overload to digitalPinToInterrupt().
+ * @tparam PIN is the Arduino pin number
+ * @return interrupt number
+ */
 template<std::uint8_t PIN>
-int constexpr digitalPinToInterrupt(const DigitalPin<PIN>& gpio)
+int constexpr digitalPinToInterrupt(const GpioPin<PIN>& gpio)
 {
-	return digitalPinToInterruptWrapper(gpio.pin);
+	return digitalPinToInterrupt(gpio.pin);
 }
 
+/**
+ * Attach function to be called on external interrupt.
+ *
+ * @param gpio is the input pin to attach interrupt to
+ * @param interruptServiceRoutine is the function to be called when interrupt occurs
+ * @param mode may be LOW, CHANGE, RISING, FALLING (see Arduino.h)
+ * @tparam PIN is the Arduino pin number
+ */
 template<std::uint8_t PIN>
-void attachInterrupt(const DigitalPin<PIN>& gpio, void (* const interruptServiceRoutine)(void), const int mode)
+void attachInterrupt(const GpioPin<PIN>& gpio, void (* const interruptServiceRoutine)(void), const int mode)
 {
 	attachInterrupt(digitalPinToInterrupt(gpio.pin), interruptServiceRoutine, mode);
 }
 
+/**
+ * Sets PWM duty cycle to pin.
+ * @tparam PIN is the Arduino pin number
+ * @param gpio output pin to set duty cycle to.
+ * @param dutyCycle between 0 and 255
+ */
+template<std::uint8_t PIN>
+void analogWrite(const GpioPin<PIN>& gpio, const int dutyCycle)
+{
+	analogWrite(gpio.pin, dutyCycle);
+}
 
-#endif /* ROBOT_CONTROL_SRC_DIGITALPIN_HPP_ */
+/** @} */ /* End group for function overloads */
+
+#endif /* GPIOPIN_HPP_ */
