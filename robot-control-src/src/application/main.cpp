32c665024c9e54bc1171560ca6cc80aaa5bb7733
David Hebbeker <david@hebbeker.info>
2021-10-30 12:34:05 +0200

    Fix error in calculation of angle.

--- robot-control-src/src/application/main.cpp
+++ robot-control-src/src/application/main.cpp
@@ -139,8 +139,8 @@ static void followWall()
         else
         {
           const Distance oppositeSide = wallDistances.at(wallDistances.max_size() - 1) - wallDistances.at(0);
-          const float angle = std::atan(static_cast<float>(oppositeSide) / static_cast<float>(path))
-              * numbers::pi / 180.0;
+          const float angle = std::atan(static_cast<float>(oppositeSide) / static_cast<float>(path)) * 180.0
+              / numbers::pi;
           drives::rotate(angle, drives::cruiseSpeed);
           lastMoveWasTurn = true;
 #if defined(DEBUG_WALL)
