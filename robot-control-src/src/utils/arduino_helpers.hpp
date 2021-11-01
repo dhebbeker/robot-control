3276d48466d9adb7208355476b0821046b4c0e9b
David Hebbeker <david@hebbeker.info>
2021-11-01 18:04:46 +0100

    Add macro for a polling wait.

--- robot-control-src/src/utils/arduino_helpers.hpp
+++ robot-control-src/src/utils/arduino_helpers.hpp
@@ -5,4 +5,35 @@
 
 using Milliseconds = decltype(millis());
 
+/**
+ * Returns the current function for given duration.
+ *
+ * First time this is passed a time measurement is started.
+ * As long as the measured time is not greater than the given duration
+ * this does return the current function.
+ *
+ * When the duration is exceeded, the timer is reset and no return is executed.
+ *
+ * This macro can be used to implement a polling timer.
+ */
+#define WAIT_AND_RETURN( duration ) \
+{ \
+  static bool waitStarted = false; \
+  static Milliseconds startTime; \
+  if(!waitStarted) \
+  { \
+    startTime = millis(); \
+    waitStarted = true; \
+  } \
+ \
+  if(millis() > startTime + duration) \
+  { \
+    waitStarted = false; \
+  } \
+  else \
+  { \
+    return; \
+  } \
+}
+
 #endif /* UTILS_ARDUINO_HELPERS_HPP_ */
