b8acfa8ef1be541fc9b9c5477d373567cc4d4538
David Hebbeker <david@hebbeker.info>
2021-11-28 10:52:01 +0100

    Automatic formatting.

--- robot-control-src/src/application/Bearing.cpp
+++ robot-control-src/src/application/Bearing.cpp
@@ -62,65 +62,65 @@ Lost::Lost(Bearing &context) : Bearing::State(context)
 
 void Lost::operation()
 {
-if (numberOfScan <= maxNumberOfScans)
-{
-  if (isIdle() && !board::isBumperPressed())
+  if (numberOfScan <= maxNumberOfScans)
   {
-    board::Distance currentMinDistance = std::numeric_limits<board::Distance>::max();
-    Counter currentMinOrientation = 0;
-    const auto &distances = board::getDistances();
-
-    // check the shortest distance of all sensors
-    for (Counter i = 0; i < size(distances); i++)
+    if (isIdle() && !board::isBumperPressed())
     {
-      if (distances[i] < currentMinDistance && distances[i] != board::distanceErrorValue)
+      board::Distance currentMinDistance = std::numeric_limits<board::Distance>::max();
+      Counter currentMinOrientation = 0;
+      const auto &distances = board::getDistances();
+
+      // check the shortest distance of all sensors
+      for (Counter i = 0; i < size(distances); i++)
       {
-        currentMinOrientation = i;
-        currentMinDistance = distances[i];
+        if (distances[i] < currentMinDistance && distances[i] != board::distanceErrorValue)
+        {
+          currentMinOrientation = i;
+          currentMinDistance = distances[i];
+        }
       }
-    }
 
-    // compare with previous scans
-    if (currentMinDistance < minDistance)
-    {
-      minDistance = currentMinDistance;
-      orientationToMinDistance = currentMinOrientation;
-      rotationToMinDistance = numberOfScan;
+      // compare with previous scans
+      if (currentMinDistance < minDistance)
+      {
+        minDistance = currentMinDistance;
+        orientationToMinDistance = currentMinOrientation;
+        rotationToMinDistance = numberOfScan;
+      }
+      rotateCounter(1, cruiseSpeed, true);
+      numberOfScan++;
     }
-    rotateCounter(1, cruiseSpeed, true);
-    numberOfScan++;
   }
-}
-else
-{
-  float angleToSensor = 0;
-  switch (orientationToMinDistance)
+  else
   {
-  case 0:
-  case 1:
-    angleToSensor = 0;
-    break;
-  case 2:
-    angleToSensor = -90;
-    break;
-  case 3:
-    angleToSensor = 90;
-    break;
-  default:
-    break;
+    float angleToSensor = 0;
+    switch (orientationToMinDistance)
+    {
+    case 0:
+    case 1:
+      angleToSensor = 0;
+      break;
+    case 2:
+      angleToSensor = -90;
+      break;
+    case 3:
+      angleToSensor = 90;
+      break;
+    default:
+      break;
+    }
+    const float longAngle = static_cast<float>(rotationToMinDistance) / stepsPerDeg + angleToSensor;
+    const PolarVector vectorToBlip =
+    { .angle = shortenAngle(longAngle), .length = minDistance };
+    Serial.printf(
+                  "Found blip at %imm, with sensor %u and scan %u/%u resulting in long angle %f\n",
+                  minDistance,
+                  orientationToMinDistance + 1,
+                  rotationToMinDistance,
+                  maxNumberOfScans,
+                  longAngle);
+    context.setState(new AligningToWall(context, vectorToBlip));
   }
-  const float longAngle = static_cast<float>(rotationToMinDistance) / stepsPerDeg + angleToSensor;
-  const PolarVector vectorToBlip =
-  { .angle = shortenAngle(longAngle), .length = minDistance };
-  Serial.printf(
-                "Found blip at %imm, with sensor %u and scan %u/%u resulting in long angle %f\n",
-                minDistance,
-                orientationToMinDistance + 1,
-                rotationToMinDistance,
-                maxNumberOfScans,
-                longAngle);
-  context.setState(new AligningToWall(context, vectorToBlip));
-}
 }
 
 FollowingWall::FollowingWall(Bearing &context) : Bearing::State(context)
