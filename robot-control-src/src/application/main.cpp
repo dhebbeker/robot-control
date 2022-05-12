d35f15c80ba6975069ba63921d71f2d55e63218d
David Hebbeker <david@hebbeker.info>
2022-05-12 22:23:53 +0200

    Re-added webserver.
    This reverts commit 8979a3042e51abb840871faab4e669606a54a4cd.

--- robot-control-src/src/application/main.cpp
+++ robot-control-src/src/application/main.cpp
@@ -1,21 +1,39 @@
 #include "main.hpp"
 #include "board.hpp"
 #include "Drives.hpp"
+#include "wifi_ap.hpp"
+#include "WebserverHandle.hpp"
 #include "../utils/array.hpp"
-#include "../utils/circular_buffer.hpp"
-#include "../utils/arduino_helpers.hpp"
-#include "../utils/Debug.hpp"
 #include "Bearing.hpp"
+#include <ESP8266WebServer.h>
 #include <type_traits>
+
+static EnvironmentRecord environmentRecord { };
+static ESP8266WebServer server(80);
+static WebserverHandle webserverHandle(server, environmentRecord);
 
 void main::setup(const char * const programIdentificationString)
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(100);
   Serial.flush();
   delay(100);
   Serial.printf("\n begin program '%s'\n", programIdentificationString);
   board::setup(drives::stopDrives);
+
+  Serial.printf("connect to wifi %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+
+  server.begin();
+  Serial.printf("webserver has IP %s\n", WiFi.localIP().toString().c_str());
+  server.on("/", std::bind(&WebserverHandle::handleRoot, &webserverHandle)); // @suppress("Invalid arguments")
+  webserverHandle.setup();
 }
 
 void main::loop()
@@ -26,6 +44,32 @@ void main::loop()
   board::setDebugLed(board::isBumperPressed(), board::DebugLeds::red);
   board::setDebugLed(!drives::isIdle(), board::DebugLeds::yellow);
   board::setDebugLed(drives::isIdle(), board::DebugLeds::green);
+
+  server.handleClient();
+  //Serial.printf("left: \t%3u, right: \t%3u\n", drives::LeftDrive::counter, drives::RightDrive::counter);
+  if(drives::LeftDrive::isIdle && drives::RightDrive::isIdle)
+  {
+    const Position newPositionCandidate = drives::flushCurrentPosition();
+    if(environmentRecord.positions[environmentRecord.positionIndex] != newPositionCandidate)
+    {
+      environmentRecord.positions[++environmentRecord.positionIndex] = newPositionCandidate;
+      environmentRecord.positionIndex %= environmentRecord.numberOfPositions;
+      webserverHandle.loop();
+    }
+    const auto newTarget = webserverHandle.flushTargetRequest();
+    if(newTarget.isTargetNew)
+    {
+      const bool bumperIsPressed = digitalRead(board::leftBumper) == LOW || digitalRead(board::rightBumper) == LOW;
+      if(newTarget.newDrive!=0 && (!newTarget.forward || !bumperIsPressed))
+      {
+        drives::driveCounter(newTarget.newDrive, drives::cruiseSpeed, !newTarget.forward);
+      }
+      else if(newTarget.newRotate!=0 && !bumperIsPressed)
+      {
+        drives::rotateCounter(newTarget.newRotate, drives::cruiseSpeed, newTarget.clockwise);
+      }
+    }
+  }
 
   yield(); // Give a time for ESP
 
