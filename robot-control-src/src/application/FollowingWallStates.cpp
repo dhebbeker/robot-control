0c2e252d2350ab2b9a82af58104408c48b8d731e
David Hebbeker <david@hebbeker.info>
2022-05-22 17:22:11 +0200

    Begin to program algorithm 'follow wall' with controller.

--- robot-control-src/src/application/FollowingWallStates.cpp
+++ robot-control-src/src/application/FollowingWallStates.cpp
@@ -1,10 +1,22 @@
 #include "FollowingWallStates.hpp"
 #include "Bearing.hpp"
 #include "../utils/Debug.hpp"
+#include "board.hpp"
 
 #define PRINT_NUMBER( x ) DEBUG_MSG_VERBOSE("Number %s = %f", #x, static_cast<double>(x))
 
-FollowingWallState1::FollowingWallState1()
+// Specify the links and initial tuning parameters
+constexpr double Kp = 2, Ki = 5, Kd = 1;
+static PID_v2 FollowingWallState1::controller(Kp, Ki, Kd, PID::Direct);
+static bool FollowingWallState1::controllerActive = false;
+
+FollowingWallState1::FollowingWallState1() : distanceValid(board::retrieveSensorStatusOrError(DistanceSensorIndex::right, this->distanceAtLastPoint))
+{
+  PRINT_CHECKPOINT();
+
+}
+
+FollowingWallState1::FollowingWallState1(const Distance distanceAtLastPoint) : distanceAtLastPoint(distanceAtLastPoint)
 {
   PRINT_CHECKPOINT();
 }
@@ -16,5 +28,18 @@ FollowingWallState1::FollowingWallState1()
  */
 PollingStateMachine::State* FollowingWallState1::operation()
 {
+  Distance currentDistance;
+  const bool distanceValid = board::retrieveSensorStatusOrError(DistanceSensorIndex::right, currentDistance);
+
+  if(!distanceValid) return new Lost();
+
+  if(!controllerActive)
+  {
+    constexpr Distance targetDistance = 100; //< [mm]
+    controller.Start(currentDistance, 0, targetDistance);
+    controllerActive = true;
+  }
+
+  const Distance error = targetDistance - currentDistance;
   return this;
 }
--- robot-control-src/src/application/FollowingWallStates.hpp
+++ robot-control-src/src/application/FollowingWallStates.hpp
@@ -1,11 +1,18 @@
 #pragma once
 
 #include "../utils/PollingStateMachine.hpp"
+#include "../middleware/EnvironmentRecord.hpp"
+#include <PID_v2.h>
 
 class FollowingWallState1 : public PollingStateMachine::State
 {
 public:
   FollowingWallState1();
+  FollowingWallState1(const Distance distanceAtLastPoint);
 private:
   virtual PollingStateMachine::State* operation() override;
+  Distance distanceAtLastPoint;
+  const bool distanceValid;
+  static PID_v2 controller;
+  static bool controllerActive;
 };
